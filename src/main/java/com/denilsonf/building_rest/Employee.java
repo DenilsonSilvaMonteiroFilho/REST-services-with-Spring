479994ee2255152a4264d73b98c19256ee86b050
DenilsonSilvaMonteiroFilho <49108665+DenilsonSilvaMonteiroFilho@users.noreply.github.com>
2021-11-22 23:10:02 -0300

    Classe de funcionario agora pode lidar com atualizacao de novos e antigos funcio

--- src/main/java/com/denilsonf/building_rest/Employee.java
+++ src/main/java/com/denilsonf/building_rest/Employee.java
@@ -7,67 +7,84 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @Entity
-public class Employee {
-    
-    private @Id @GeneratedValue Long id;
-    private String name;
-    private String role;
+class Employee {
 
-    Employee(){
+  private @Id @GeneratedValue Long id;
+  private String firstName;
+  private String lastName;
+  private String role;
 
-    }
+  Employee() {}
 
-    Employee(String name, String role){
-        this.name = name;
-        this.role = role;
-    }
+  Employee(String firstName, String lastName, String role) {
 
-    public Long getId(){
-        return this.id;
-    }
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.role = role;
+  }
 
-    public String getName(){
-        return this.name;
-    }
+  public String getName() {
+    return this.firstName + " " + this.lastName;
+  }
 
-    public String getRole(){
-        return this.role;
-    }
+  public void setName(String name) {
+    String[] parts = name.split(" ");
+    this.firstName = parts[0];
+    this.lastName = parts[1];
+  }
 
-    public void setId(Long newId){
-        this.id = newId;
-    }
-    
-    public void setName(String newName){
-        this.name = newName;
-    }
+  public Long getId() {
+    return this.id;
+  }
 
-    public void setRole(String newRole){
-        this.role = newRole;
-    }
+  public String getFirstName() {
+    return this.firstName;
+  }
 
-    @Override
-    public boolean equals(Object o){
-        if(this == o){
-            return true;
-        }
-        if(!(o instanceof Employee)){
-            return false;
-        }
-        Employee employee = (Employee) o;
-        return Objects.equals(this.id, employee.id) && 
-    Objects.equals(this.name, employee.name)
-            && Objects.equals(this.role, employee.role);
-    }
+  public String getLastName() {
+    return this.lastName;
+  }
 
-    @Override
-    public int hashCode(){
-        return Objects.hash(this.id, this.name, this.role);
-    }
+  public String getRole() {
+    return this.role;
+  }
 
-    @Override
-    public String toString(){
-        return "Employee{" + "id=" + this.id + ", name='" + this.name + 
-        '\'' + ", role='" + this.role + '\'' + '}';
-    }
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+
+  public void setRole(String role) {
+    this.role = role;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+
+    if (this == o)
+      return true;
+    if (!(o instanceof Employee))
+      return false;
+    Employee employee = (Employee) o;
+    return Objects.equals(this.id, employee.id) && Objects.equals(this.firstName, employee.firstName)
+        && Objects.equals(this.lastName, employee.lastName) && Objects.equals(this.role, employee.role);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(this.id, this.firstName, this.lastName, this.role);
+  }
+
+  @Override
+  public String toString() {
+    return "Employee{" + "id=" + this.id + ", firstName='" + this.firstName + '\'' + ", lastName='" + this.lastName
+        + '\'' + ", role='" + this.role + '\'' + '}';
+  }
 }
