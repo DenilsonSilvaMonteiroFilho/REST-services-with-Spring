2d3b864d278344b1d1f10c84a01b377d07dd576f
DenilsonSilvaMonteiroFilho <49108665+DenilsonSilvaMonteiroFilho@users.noreply.github.com>
2021-11-24 23:16:43 -0300

    Tratamento de solicitacoes para DELETE

--- src/main/java/com/denilsonf/building_rest/EmployeeController.java
+++ src/main/java/com/denilsonf/building_rest/EmployeeController.java
@@ -80,7 +80,10 @@ class EmployeeController {
   }
 
   @DeleteMapping("/employees/{id}")
-  void deleteEmployee(@PathVariable Long id) {
-    repository.deleteById(id);
+  ResponseEntity<?> deleteEmployee(@PathVariable Long id) {
+
+  repository.deleteById(id);
+
+  return ResponseEntity.noContent().build();
   }
 }
